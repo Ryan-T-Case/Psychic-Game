5c229700ee86a6fb014967a89b1c622c5e27dade
Ryan Case <Ryan_Case@outlook.com>
2018-08-15 01:17:22 -0400

    Pseudocoded the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//There are wins, losses, guesses left, and guesses so far
+
+//Computer thinks of a random lowercase letter
+
+//Player inputs a letter to guess what computer is thinking
+
+//Game checks if that letter matches the computer's letter
+
+//If the letter matches, add a win to the Wins total
+
+//If the letter does not match, deduct from the guesses left total
+
+//If guesses left reaches 0, add a loss to the losses total
+
+//If a win or loss occurs, reset the game.
