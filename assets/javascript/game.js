943b4f18690f0695cde015a7736ef1463caaac52
Ryan Case <Ryan_Case@outlook.com>
2018-08-18 00:30:45 -0400

    Game is now fully functional and manipulates the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,38 +6,38 @@ var guessesMade = [];
 var computerLetter = 0;
 // There is a list of all possible letters to choose from
 var randomLetters = "abcdefghijklmnopqrstuvwxyz".split("");
-console.log(randomLetters);
 //Computer thinks of a random lowercase letter
 var computerLetter = randomLetters[Math.floor(Math.random() * randomLetters.length)];
 console.log("Guess this letter: " + computerLetter);
 //Player inputs a letter to guess what computer is thinking
 document.onkeyup = function (event) {
-    var userGuess = event.key;
-    console.log("User Guess: " + userGuess);
+    var userGuess = event.key.toLowerCase();
     //Game checks if that letter matches the computer's letter
     if (userGuess === computerLetter) {
         //If the letter matches, add a win to the Wins total, reset computer letter and guesses.
         wins++;
-        alert("Total wins: " + wins);
+        document.getElementById("winCount").innerHTML = wins;
         guessesLeft = 9;
+        guessesMade.length = 0;
         computerLetter = randomLetters[Math.floor(Math.random() * randomLetters.length)];
         console.log("Guess new letter: " + computerLetter);
     }
     //If the letter does not match, deduct from the guesses left total and add the letter to gueses so far
     else {
         guessesLeft--;
-        alert("Guesses Left: " + guessesLeft);
-        // Guesses so far should not allow repeat of the same letter
-        if ((guessesMade.indexOf(userGuess) === -1) && (userGuess !== "Enter" && userGuess !== "Shift" && userGuess !=="Backspace")) {
+        document.getElementById("attemptsCount").innerHTML = guessesLeft;
+        // Guesses so far should not allow repeat of the same letter, also does not allow non-letter keys
+        if ((guessesMade.indexOf(userGuess) === -1) && (userGuess !== "Enter" && userGuess !== "Shift" && userGuess !=="Backspace" && userGuess !=="shift")) {
             guessesMade.push(userGuess);
-            console.log(guessesMade);
+            document.getElementById("guessLog").innerHTML = guessesMade;
         }
     }
     //If guesses left reaches 0, add a loss to the losses total, reset computer letter and guesses.
     if (guessesLeft <= 0) {
         losses++;
-        alert("You lose! Total Losses: " + losses);
+        document.getElementById("lossCount").innerHTML = losses;
         guessesLeft = 9;
+        guessesMade.length = 0;
         computerLetter = randomLetters[Math.floor(Math.random() * randomLetters.length)];
         console.log("Guess new letter: " + computerLetter);
     }
